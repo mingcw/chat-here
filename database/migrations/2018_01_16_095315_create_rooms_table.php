82cecfa4620899e8af91a58f7648203f4a14c413
mingcw <mingc2016@126.com>
2018-01-16 22:11:25 +0800

    添加一个表字段

--- database/migrations/2018_01_16_095315_create_rooms_table.php
+++ database/migrations/2018_01_16_095315_create_rooms_table.php
@@ -18,9 +18,10 @@ class CreateRoomsTable extends Migration
             $table->string('name', 20)->default('')->comment('房间名');
             $table->string('description', 140)->nullable()->default('')->comment('房间描述');
             $table->unsignedInteger('capacity')->default(0)->comment('房间容量');
-            $table->string('username', 140)->default('')->comment('所属用户名');
+            $table->unsignedInteger('number')->default(0)->comment('当前人数');
+            $table->string('username', 140)->default('')->comment('房主名');
         });
-    }
+    }               
 
     /**
      * Reverse the migrations.
