c8d71932c1f5b629bed23e2c475eb1a1d48132b6
mingcw <mingc2016@126.com>
2018-01-19 02:06:52 +0800

    GatewayWorker添加连接关闭事件

--- gatewayworker/app/chat/Events.php
+++ gatewayworker/app/chat/Events.php
@@ -12,9 +12,33 @@ class Events
         )));
     }
 
-    // GatewayWorker建议不做任何业务逻辑，onMessage留空即可
+    /**
+     * 有消息时触发该方法。GatewayWorker建议不做任何业务逻辑，onMessage留空即可
+     * @param  [type] $client_id 发消息的$client_id
+     * @param  [type] $message   消息
+     * @return [type]            [description]
+     */
     public static function onMessage($client_id, $message)
     {
 
+    }
+
+    /**
+     * 当用户断开连接时触发的方法
+     * @param integer $client_id 断开连接的客户端client_id
+     * @return void
+     */
+    public static function onClose($client_id)
+    {
+        // 房间广播有连接关闭的信号
+        $room_id = $_SESSION['room_id'];
+        $uname   = $_SESSION['uname'];
+
+        if (Gateway::getClientCountByGroup($room_id)) {
+            Gateway::sendToGroup($room_id, json_encode(array(
+                'type'      => 'close',
+                'uname'     => $uname
+            )));
+        }   
     }
 }
